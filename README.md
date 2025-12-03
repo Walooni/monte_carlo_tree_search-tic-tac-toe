fa93af16daaf36c4c880b202ce584d6dcd2543a2
Walooni <id.yahyair@gmail.com>
2025-12-03 18:02:24 +0700

    feat : choosing first

--- README.md
+++ README.md
@@ -1,76 +1,46 @@
-# monte-carlo-tree-search
+# Monte Carlo Tree Search – Tic Tac Toe
 
-Simple Game about Tic Tac Toe with Monte Carlo Tree Search.
+Sebuah implementasi sederhana permainan **Tic Tac Toe** menggunakan **Monte Carlo Tree Search (MCTS)**.  
+Proyek ini bertujuan memberikan pemahaman intuitif tentang bagaimana MCTS bekerja melalui proses eksplorasi, simulasi, dan pengambilan keputusan berbasis probabilitas.  
+Contoh ini dapat dijadikan dasar untuk mempelajari atau mengembangkan MCTS pada game dan proyek AI lainnya.
 
-Just like below:
+---
 
-![](./images/1.png)
+## 🚀 Setup
 
-## Run
+Sebelum menjalankan aplikasi, pastikan Anda sudah menginstal **NumPy**, karena proyek ini menggunakannya untuk perhitungan simulasi MCTS.
 
-run the code:
+Install NumPy:
 
+```bash
+pip install numpy
 ```
+
+## ▶️ Menjalankan Program
+
+Clone repository dan jalankan aplikasi:
+```bash
 git clone https://github.com/cryer/monte-carlo-tree-search.git
 cd monte-carlo-tree-search
-python run.py
+python runV2.py
 ```
 
-## Note
+## 📊 Contoh Hasil Permainan
 
-As we all know,in Tic Tac Toe game,the starting player never lose,at least tie,asuming both are master.
-But someone may not know that the first best move of the starting player is not the middle,but the corner.
-MCTS also confirms this,but that's just for probability，for masters they always tie.
-
-* ```I made this small game only for showing how Monte Carlo Tree Search works,you may focus on mcts and apply it to your only apps```
-
-## Results
-
-I just show you the process,do not mind my move.
-```
-    0   |   _       _       _    
-    1   |   _       X       _    
-    2   |   _       _       _    
+    0   |   _       _       _
+    1   |   _       X       _
+    2   |   _       _       _
 ______________________________
 Your move: 0,0
 
-    0   |   O       _       _    
-    1   |   _       X       _    
-    2   |   _       _       _    
+    0   |   O       _       _
+    1   |   _       X       _
+    2   |   _       _       _
 ______________________________
-
-    0   |   O       _       _    
-    1   |   X       X       _    
-    2   |   _       _       _    
-______________________________
-Your move: 1,2
-
-    0   |   O       _       _    
-    1   |   X       X       O    
-    2   |   _       _       _    
-______________________________
-
-    0   |   O       X       _    
-    1   |   X       X       O    
-    2   |   _       _       _    
-______________________________
-Your move: 2,0
-
-    0   |   O       X       _    
-    1   |   X       X       O    
-    2   |   O       _       _    
-______________________________
-
-    0   |   O       X       _    
-    1   |   X       X       O    
-    2   |   O       X       _    
-______________________________
+...
 You lose!
 
-```
-MCTS is the starting player,it's also very easy for you to change code to set human as the starting player.Just modify run.py.
-
-## Inspiration
-
+🔗 Refrence
+* [cryer](https://github.com/cryer/monte-carlo-tree-search)
 * [int8](https://github.com/int8/monte-carlo-tree-search)
 
---
+++ runV2.py
@@ -0,0 +1,137 @@
+import numpy as np
+
+from mcts.nodes import MonteCarloTreeSearchNode
+from mcts.search import MonteCarloTreeSearch
+from tictactoe import TicTacToeGameState, TicTacToeMove
+
+board_size = 3
+depth = 1000
+
+
+def draw_chessboard(board):
+    """
+    """
+    board_size = board.shape[0]
+
+    for i in range(board_size):
+        print("")
+        # nomor baris di sebelah kiri
+        print("{0:3}".format(i).center(8) + "|", end="")
+        for j in range(board_size):
+            cell = board[i][j]
+            if cell == 0:
+                symbol = "_"
+            elif cell == 1:
+                symbol = "X"
+            else:
+                symbol = "O"
+            print(symbol.center(8), end="")
+    print("\n" + "_" * (10 + board_size * 8))
+
+
+def get_human_action(state):
+    """
+    Minta input langkah manusia dalam format: row,col (contoh: 0,2)
+    """
+    location = input("Your move (row,col): ")
+
+    try:
+        x_str, y_str = location.split(",")
+        x = int(x_str)
+        y = int(y_str)
+    except ValueError:
+        print("Format salah. Contoh yang benar: 0,2")
+        return get_human_action(state)
+
+    # manusia = O = -1
+    move = TicTacToeMove(x, y, TicTacToeGameState.o)
+
+    if not state.is_move_legal(move):
+        print("invalid move")
+        return get_human_action(state)
+
+    return move
+
+
+def is_game_over(state):
+    """
+    Cek apakah game sudah selesai dan print hasilnya.
+    Return True kalau selesai, False kalau belum.
+    """
+    if state.is_game_over():
+        if state.game_result == 1.0:
+            print("You lose!")
+        elif state.game_result == 0.0:
+            print("Tie!")
+        elif state.game_result == -1.0:
+            print("You Win!")
+        return True
+    return False
+
+
+def choose_starting_player():
+    """
+    Menu awal: pilih siapa yang jalan duluan.
+    Return: "computer" atau "human"
+    """
+    print("Pilih siapa yang jalan duluan:")
+    print("1. Komputer (X)")
+    print("2. Manusia  (O)")
+
+    while True:
+        choice = input("Masukkan pilihan [1/2]: ").strip()
+        if choice == "1":
+            return "computer"
+        if choice == "2":
+            return "human"
+        print("Pilihan tidak valid, silakan pilih 1 atau 2.")
+
+
+def init_state(starting_player: str):
+    """
+    Inisialisasi state awal berdasarkan siapa yang mulai.
+    - Jika 'computer'  -> komputer (X = +1) langsung ambil langkah pertama dengan MCTS.
+    - Jika 'human'     -> papan kosong, giliran pertama milik manusia (O = -1).
+    """
+    state = np.zeros((board_size, board_size))
+
+    if starting_player == "computer":
+        # komputer (X) mulai duluan
+        initial_board_state = TicTacToeGameState(state=state, next_to_move=TicTacToeGameState.x)
+        root = MonteCarloTreeSearchNode(state=initial_board_state, parent=None)
+        mcts = MonteCarloTreeSearch(root)
+        best_node = mcts.best_action(depth)  # jumlah simulasi MCTS
+        return best_node.state
+    else:
+        # manusia (O) mulai duluan, papan kosong
+        initial_board_state = TicTacToeGameState(state=state, next_to_move=TicTacToeGameState.o)
+        return initial_board_state
+
+
+if __name__ == "__main__":
+    # 1. Pilih siapa yang jalan duluan
+    starting_player = choose_starting_player()
+
+    # 2. Inisialisasi state awal
+    current_state = init_state(starting_player)
+    draw_chessboard(current_state.board)
+
+    # 3. Loop utama permainan
+    while not is_game_over(current_state):
+
+        # --- Giliran manusia ---
+        human_move = get_human_action(current_state)
+        current_state = current_state.move(human_move)
+        draw_chessboard(current_state.board)
+
+        if is_game_over(current_state):
+            break
+
+        # --- Giliran komputer (MCTS) ---
+        # Dari papan sekarang, set next_to_move = X (komputer)
+        board_state = TicTacToeGameState(state=current_state.board, next_to_move=TicTacToeGameState.x)
+        root = MonteCarloTreeSearchNode(state=board_state, parent=None)
+        mcts = MonteCarloTreeSearch(root)
+        best_computer_node = mcts.best_action(depth)
+        current_state = best_computer_node.state
+        draw_chessboard(current_state.board)
